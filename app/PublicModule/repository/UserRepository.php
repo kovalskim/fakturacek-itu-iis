858c08accfac0dd1f59abc9cef9183365eb40f24
radekjuzl <juzlradek@gmail.com>
2021-11-09 10:36:28 +0100

    New SQL query

--- app/PublicModule/repository/UserRepository.php
+++ app/PublicModule/repository/UserRepository.php
@@ -12,4 +12,27 @@ class UserRepository extends AllRepository
     {
         return $this->connection->query('SELECT * FROM %table WHERE email = %s', $this->table, $email)->fetch();
     }
+
+    public function getUserByCin($cin): ?Row
+    {
+        return $this->connection->query('SELECT * FROM %table WHERE cin = %s', $this->table, $cin)->fetch();
+    }
+
+    public function insertUser($cin, $name, $email, $phone, $password, $role, $street, $city, $zip)
+    {
+        $this->connection->query('INSERT INTO %table', [
+            'cin' => $cin,
+            'name' => $name,
+            'email' => $email,
+            'phone' => $phone,
+            'password' => $password,
+            'role' => $role,
+            'street' => $street,
+            'city' => $city,
+            'zip' => $zip,
+            ]);
+        //TODO: Udelat select zda tam je? nebo vypise insert nejakou chybu?
+        return true;
+    }
+
 }
