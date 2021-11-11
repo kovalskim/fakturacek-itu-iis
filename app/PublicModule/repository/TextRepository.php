160c1de594792659c2252d944bbf8cbc68b45b88
radekjuzl <juzlradek@gmail.com>
2021-11-11 12:17:00 +0100

    Add text repository

---
+++ app/PublicModule/repository/TextRepository.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\PublicModule\repository;
+
+class TextRepository extends AllRepository
+{
+    private $table = "texts";
+
+    public function getTextByType($type)
+    {
+        return $this->connection->query("SELECT text FROM %table WHERE type = %s", $this->table, $type)->fetchField();
+    }
+
+
+
+}
