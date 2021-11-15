86e88d9fbf40ec61ac78fdf741373180af848877
radekjuzl <juzlradek@gmail.com>
2021-11-15 20:32:23 +0100

    Change password in admin

---
+++ app/AdminModule/presenters/ChangePasswordPresenter.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\AdminModule\presenters;
+
+use App\PublicModule\forms\LogInFormFactory;
+use App\PublicModule\model\UserManager;
+use Nette\Application\AbortException;
+use Nette\Application\UI\Form;
+use Exception;
+
+final class ChangePasswordPresenter extends BasePresenter
+{
+    /** @var LogInFormFactory */
+    private $logInFormFactory;
+
+    /** @var UserManager */
+    private $userManager;
+
+    public function __construct(LogInFormFactory $logInFormFactory, UserManager $userManager)
+    {
+        parent::__construct();
+        $this->logInFormFactory = $logInFormFactory;
+        $this->userManager = $userManager;
+    }
+
+    public function actionDefault()
+    {
+
+    }
+
+    protected function createComponentChangePasswordForm(): Form
+    {
+        $form = $this->logInFormFactory->createChangePasswordForm();
+        $form->onSuccess[] = [$this, "changePasswordFormSucceeded"];
+        return $form;
+    }
+
+    /**
+     * @throws AbortException
+     */
+    public function changePasswordFormSucceeded($form, $values)
+    {
+        try
+        {
+            $this->userManager->changePasswordFormSucceeded($form, $values);
+            $this->flashMessage('Heslo bylo úspěšně změněno.', 'success');
+        }
+        catch (Exception $e)
+        {
+            $this->flashMessage($e->getMessage(), 'danger');
+        }
+        $this->redirect('this');
+    }
+
+
+}
