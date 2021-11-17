dd1579344b0c8710c74260bb871aa83aba47f685
radekjuzl <juzlradek@gmail.com>
2021-11-17 10:40:20 +0100

    Update database

--- fakturacek.sql
+++ fakturacek.sql
@@ -94,12 +94,13 @@ CREATE TABLE `texts` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `type` varchar(255) COLLATE utf8mb4_czech_ci NOT NULL,
   `text` text COLLATE utf8mb4_czech_ci NOT NULL,
+  `img_path` varchar(255) COLLATE utf8mb4_czech_ci NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_czech_ci;
 
-INSERT INTO `texts` (`id`, `type`, `text`) VALUES
-(1,	'aboutus',	'<h1>Nadpis</h1>\r\n<p>Odstavec s <b>tučným textem</b>, s <i>kurzivou</i>.\r\n<span style=\"color: red;\">Červený text.</span></p>\r\n<h2>Nadpis druhé úrovně</h2>\r\n<p>Odstavec s <i><b>tučnou kurzivou.</b></i><br>\r\nText po zalomení řádku patří do téhož odstavce.</p>'),
-(2,	'contact',	'<p>Odstavec s <b>tučným textem</b>, s <i>kurzivou</i>.\r\n<span style=\"color: red;\">Červený text.</span></p>');
+INSERT INTO `texts` (`id`, `type`, `text`, `img_path`) VALUES
+(1,	'aboutus',	'<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aliquam ornare wisi eu metus. Ut tempus purus at lorem. Nullam sit amet magna in magna gravida vehicula. Suspendisse sagittis ultrices augue. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Duis sapien nunc, commodo et, interdum suscipit, sollicitudin et, dolor. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Mauris elementum mauris vitae tortor. Aenean placerat. Nam quis nulla. Cras pede libero, dapibus nec, pretium sit amet, tempor quis. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</p>\r\n\r\n<p>Duis condimentum augue id magna semper rutrum. Mauris elementum mauris vitae tortor. Quisque porta. Maecenas libero. Etiam sapien elit, consequat eget, tristique non, venenatis quis, ante. Aliquam erat volutpat. Maecenas sollicitudin. Sed vel lectus. Donec odio tempus molestie, porttitor ut, iaculis quis, sem. Duis viverra diam non justo. Aenean vel massa quis mauris vehicula lacinia. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos hymenaeos. Pellentesque arcu. Mauris suscipit, ligula sit amet pharetra semper, nibh ante cursus purus, vel sagittis velit mauris vel metus. Nullam sit amet magna in magna gravida vehicula. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In dapibus augue non sapien. Pellentesque arcu. In rutrum.</p>\r\n',	'img/aboutus.jpg'),
+(2,	'contact',	'<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aliquam ornare wisi eu metus. Ut tempus purus at lorem. Nullam sit amet magna in magna gravida vehicula. Suspendisse sagittis ultrices augue. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Duis sapien nunc, commodo et, interdum suscipit, sollicitudin et, dolor. Nam libero tempore, cum soluta nobis est.</p>\r\n',	'img/contact.jpg');
 
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
@@ -119,14 +120,16 @@ CREATE TABLE `users` (
   `street` varchar(255) COLLATE utf8mb4_czech_ci DEFAULT NULL,
   `city` varchar(255) COLLATE utf8mb4_czech_ci DEFAULT NULL,
   `zip` varchar(5) COLLATE utf8mb4_czech_ci DEFAULT NULL,
+  `verified` int(11) NOT NULL DEFAULT 0,
+  `avatar_path` varchar(255) COLLATE utf8mb4_czech_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_czech_ci;
 
-INSERT INTO `users` (`id`, `cin`, `name`, `email`, `phone`, `password`, `hash`, `hash_validity`, `role`, `deleted`, `banned`, `account_number`, `logo_path`, `street`, `city`, `zip`) VALUES
-(1,	NULL,	'Admin',	'admin@fakturacek.cz',	NULL,	'$2y$10$LtxViOPJkipfUKGGivFgle9UTcPVsm2ebuU1Jic7L.uGBgZws/FhS',	NULL,	NULL,	'admin',	0,	0,	NULL,	NULL,	NULL,	NULL,	NULL),
-(2,	NULL,	'Business',	'business@fakturacek.cz',	NULL,	'$2y$10$LtxViOPJkipfUKGGivFgle9UTcPVsm2ebuU1Jic7L.uGBgZws/FhS',	NULL,	NULL,	'business',	0,	0,	NULL,	NULL,	NULL,	NULL,	NULL),
-(3,	NULL,	'Accountant',	'accountant@fakturacek.cz',	NULL,	'$2y$10$LtxViOPJkipfUKGGivFgle9UTcPVsm2ebuU1Jic7L.uGBgZws/FhS',	NULL,	NULL,	'accountant',	0,	0,	NULL,	NULL,	NULL,	NULL,	NULL),
-(4,	'12345678',	'Radek Jůzl',	'radekjuzl@seznam.cz',	'',	'$2y$10$bRhFsdgSyzTPLLNuKj6DRO/E7vA8WBdTtmYAP3kjzpjg9ANG1dkuG',	NULL,	NULL,	'business',	0,	0,	NULL,	NULL,	'Kam 204',	'Nikam',	'39601');
+INSERT INTO `users` (`id`, `cin`, `name`, `email`, `phone`, `password`, `hash`, `hash_validity`, `role`, `deleted`, `banned`, `account_number`, `logo_path`, `street`, `city`, `zip`, `verified`, `avatar_path`) VALUES
+(1,	NULL,	'Admin',	'admin@fakturacek.cz',	NULL,	'$2y$10$oBJfrptxWy7e07knXIBFs.7E8gwpAJ5sPEAA52L98rm0rK9HXV7FO',	NULL,	NULL,	'admin',	0,	0,	NULL,	NULL,	NULL,	NULL,	NULL,	0,	NULL),
+(2,	NULL,	'Business',	'business@fakturacek.cz',	NULL,	'$2y$10$LtxViOPJkipfUKGGivFgle9UTcPVsm2ebuU1Jic7L.uGBgZws/FhS',	NULL,	NULL,	'business',	0,	0,	NULL,	NULL,	NULL,	NULL,	NULL,	0,	NULL),
+(3,	NULL,	'Accountant',	'accountant@fakturacek.cz',	NULL,	'$2y$10$KUkctHpRXI71vM41yRI/Q.Sxm3FiYF3JX6tf88qBzdwbffeuiNQ32',	NULL,	NULL,	'accountant',	0,	0,	NULL,	NULL,	NULL,	NULL,	NULL,	0,	NULL),
+(4,	'12345678',	'Radek Jůzl',	'radekjuzl@seznam.cz',	'',	'$2y$10$fIjseCS/5fFUq.Z8VNw.YOdGNV8D5po/AgmvLsjEy48Fd/ahff9E6',	NULL,	NULL,	'business',	0,	0,	NULL,	NULL,	'Kam 204',	'Nikam',	'39601',	0,	NULL);
 
 DROP TABLE IF EXISTS `users_clients`;
 CREATE TABLE `users_clients` (
@@ -164,5 +167,10 @@ CREATE TABLE `users_last_password_change` (
   CONSTRAINT `users_last_password_change_ibfk_1` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_czech_ci;
 
+INSERT INTO `users_last_password_change` (`id`, `users_id`, `timestamp`) VALUES
+(1,	4,	'2021-11-15 15:47:15'),
+(2,	4,	'2021-11-15 19:25:14'),
+(3,	1,	'2021-11-15 19:26:29'),
+(4,	3,	'2021-11-15 19:27:07');
 
--- 2021-11-12 23:39:14
+-- 2021-11-17 09:39:11
