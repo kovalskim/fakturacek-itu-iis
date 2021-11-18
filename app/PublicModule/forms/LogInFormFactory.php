b6ab8aac49a323eca07f4f696b8bc5c8c9e33cb5
radekjuzl <juzlradek@gmail.com>
2021-11-18 12:54:28 +0100

    Star for Martin

--- app/PublicModule/forms/LogInFormFactory.php
+++ app/PublicModule/forms/LogInFormFactory.php
@@ -38,40 +38,40 @@ class LogInFormFactory
     {
         $form = $this->formFactory->create();
 
-        $form->addEmail('email', 'E-mail:')
+        $form->addEmail('email', '*E-mail:')
             ->setRequired()
             ->setHtmlAttribute('placeholder', 'E-mail')
             ->setHtmlAttribute('autofocus');
 
-        $form->addPassword('password', 'Heslo:')
+        $form->addPassword('password', '*Heslo:')
             ->setRequired()
             ->addRule($form::MIN_LENGTH, 'Heslo musí mít alespoň %d znaků', 8)
             ->setHtmlAttribute('placeholder', 'Heslo');
 
-        $form->addPassword('passwordAgain', 'Heslo znovu:')
+        $form->addPassword('passwordAgain', '*Heslo znovu:')
             ->setRequired()
             ->setOmitted()
             ->addRule($form::EQUAL, 'Hesla se neshodují', $form['password'])
             ->setHtmlAttribute('placeholder', 'Heslo znovu');
 
-        $form->addText('cin', 'IČ:')
+        $form->addText('cin', '*IČ:')
             ->setRequired()
             ->addRule($form::LENGTH, 'IČ musí mít %d znaků', 8)
             ->setHtmlAttribute('placeholder', 'IČ');
 
-        $form->addText('name', 'Jméno a příjmení:')
+        $form->addText('name', '*Jméno a příjmení:')
             ->setRequired()
             ->setHtmlAttribute('placeholder', 'Jméno a příjmení');
 
-        $form->addText('street', 'Ulice a č.p.:')
+        $form->addText('street', '*Ulice a č.p.:')
             ->setRequired()
             ->setHtmlAttribute('placeholder', 'Ulice a č.p.:');
 
-        $form->addText('city', 'Město')
+        $form->addText('city', '*Město')
             ->setRequired()
             ->setHtmlAttribute('placeholder', 'Město');
 
-        $form->addText('zip', 'PSČ')
+        $form->addText('zip', '*PSČ')
             ->setRequired()
             ->setHtmlAttribute("inputmode", "numeric")
             ->setHtmlAttribute('placeholder', 'PSČ');
@@ -79,7 +79,7 @@ class LogInFormFactory
         $form->addText('phone', 'Telefon:')
             ->setHtmlAttribute('placeholder', 'Telefon');
 
-        $form->addRadioList('role', 'Role:', ["business" => 'OSVČ', "accountant" => 'Účetní'])
+        $form->addRadioList('role', '*Role:', ["business" => 'OSVČ', "accountant" => 'Účetní'])
             ->setDefaultValue("business")
             ->setRequired();
 
