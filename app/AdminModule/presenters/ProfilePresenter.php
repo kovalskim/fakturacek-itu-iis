908499afad7511b1512a54962344e8ac4bb89178
radekjuzl <juzlradek@gmail.com>
2021-11-18 20:43:52 +0100

    Add view/edit admin profile

---
+++ app/AdminModule/presenters/ProfilePresenter.php
@@ -0,0 +1,123 @@
+<?php
+
+/** Author: Radek Jůzl */
+
+namespace App\AdminModule\presenters;
+
+
+use App\PublicModule\forms\LogInFormFactory;
+use App\PublicModule\model\EditProfile;
+use App\PublicModule\model\UploadImage;
+use App\PublicModule\repository\UserRepository;
+use Exception;
+use Nette\Application\AbortException;
+use Nette\Application\UI\Form;
+use Nette\Http\Session;
+use Nette\Security\User;
+use Nette\Utils\ImageException;
+use Nette\Utils\UnknownImageFileException;
+
+final class ProfilePresenter extends BasePresenter
+{
+    /** @var User*/
+    public $user;
+
+    /** @var UserRepository */
+    private $userRepository;
+
+    /** @var LogInFormFactory */
+    private $logInFormFactory;
+
+    /** @var EditProfile */
+    private $editProfile;
+
+    /** @var UploadImage */
+    private $uploadImage;
+
+    public function __construct(User $user, UserRepository $userRepository, LogInFormFactory $logInFormFactory, EditProfile $editProfile, UploadImage $uploadImage)
+    {
+        parent::__construct();
+        $this->user = $user;
+        $this->userRepository = $userRepository;
+        $this->logInFormFactory = $logInFormFactory;
+        $this->editProfile = $editProfile;
+        $this->uploadImage = $uploadImage;
+    }
+
+    public function actionDefault()
+    {
+
+    }
+
+    public function renderDefault()
+    {
+        $this->template->profile = $this->userRepository->getUserProfile($this->user->getId());
+    }
+
+    public function actionEdit()
+    {
+        $profile = $this->userRepository->getUserProfile($this->user->getId());
+        $this->getComponent("editProfileForm")->setDefaults($profile);
+    }
+
+    protected function createComponentEditProfileForm(): Form
+    {
+        $form = $this->logInFormFactory->createEditProfileAdminForm();
+        $form->onValidate[] = [$this, "editProfileFormValidate"];
+        $form->onSuccess[] = [$this, "editProfileFormSucceeded"];
+        return $form;
+    }
+
+    public function editProfileFormValidate($form, $values)
+    {
+        $this->editProfile->editProfileFormValidate($form, $values);
+    }
+
+    /**
+     * @throws AbortException
+     */
+
+    public function editProfileFormSucceeded($form, $values)
+    {
+        $new_email = $this->editProfile->editProfileFormSucceeded($form, $values);
+        if($new_email)
+        {
+            $this->session->destroy();
+            $this->user->logout();
+            $this->flashMessage('Ověř si nový e-mail a přihlaš se s ním!', 'info');
+            $this->redirect(':Public:Homepage:default');
+        }
+
+        $this->flashMessage("Změna se provedla", "success");
+        $this->redirect(":Admin:Profile:default");
+    }
+
+    public function actionUpload()
+    {
+
+    }
+
+    protected function createComponentUploadAvatarForm(): Form
+    {
+        $form = $this->logInFormFactory->createUploadAvatarForm();
+        $form->onSuccess[] = [$this, "uploadAvatarFormSucceeded"];
+        return $form;
+    }
+
+    public function uploadAvatarFormSucceeded($form, $values)
+    {
+        try
+        {
+            $this->uploadImage->uploadAvatarFormSucceeded($form,$values);
+        }
+        catch (Exception $e)
+        {
+            $this->flashMessage($e->getMessage(), 'danger');
+            $this->redirect(":Admin:Profile:default");
+        }
+
+        $this->flashMessage("Avatar se nahrál", "success");
+        $this->redirect(":Admin:Profile:default");
+    }
+
+}
--- app/PublicModule/forms/LogInFormFactory.php
+++ app/PublicModule/forms/LogInFormFactory.php
@@ -156,24 +156,24 @@ class LogInFormFactory
     {
         $form = $this->formFactory->create();
 
-        $form->addEmail('email', 'E-mail')
+        $form->addEmail('email', '*E-mail')
             ->setRequired()
             ->setHtmlAttribute('placeholder', 'E-mail')
             ->setHtmlAttribute('autofocus');
 
-        $form->addText('name', 'Jméno a příjmení:')
+        $form->addText('name', '*Jméno a příjmení:')
             ->setRequired()
             ->setHtmlAttribute('placeholder', 'Jméno a příjmení');
 
-        $form->addText('street', 'Ulice a č.p.:')
+        $form->addText('street', '*Ulice a č.p.:')
             ->setRequired()
             ->setHtmlAttribute('placeholder', 'Ulice a č.p.:');
 
-        $form->addText('city', 'Město')
+        $form->addText('city', '*Město')
             ->setRequired()
             ->setHtmlAttribute('placeholder', 'Město');
 
-        $form->addText('zip', 'PSČ')
+        $form->addText('zip', '*PSČ')
             ->setRequired()
             ->setHtmlAttribute("inputmode", "numeric")
             ->setHtmlAttribute('placeholder', 'PSČ');
@@ -181,7 +181,29 @@ class LogInFormFactory
         $form->addText('phone', 'Telefon:')
             ->setHtmlAttribute('placeholder', 'Telefon');
 
-        $form->addSubmit('send', 'Odeslat');
+        $form->addSubmit('send', 'Uložit změny');
+
+        return $form;
+    }
+
+    /** Author: Radek Jůzl */
+    public function createEditProfileAdminForm(): Form
+    {
+        $form = $this->formFactory->create();
+
+        $form->addEmail('email', '*E-mail')
+            ->setRequired()
+            ->setHtmlAttribute('placeholder', 'E-mail')
+            ->setHtmlAttribute('autofocus');
+
+        $form->addText('name', '*Jméno a příjmení:')
+            ->setRequired()
+            ->setHtmlAttribute('placeholder', 'Jméno a příjmení');
+
+        $form->addText('phone', 'Telefon:')
+            ->setHtmlAttribute('placeholder', 'Telefon');
+
+        $form->addSubmit('send', 'Uložit změny');
 
         return $form;
     }
@@ -196,7 +218,7 @@ class LogInFormFactory
             ->addRule($form::MAX_FILE_SIZE, 'Maximální velikost je 5 MB.', 1024 * 1024 * 5)
             ->setRequired();
 
-        $form->addSubmit('send', 'Odeslat');
+        $form->addSubmit('send', 'Nahrát fotku');
 
         return $form;
     }
