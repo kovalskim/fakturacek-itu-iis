ab8346836a4ea84a0552f2984cb10c9e05072d43
Martin Kovalski <martin.kovalski@rekode.cz>
2021-11-24 16:25:35 +0100

    edit form clients

--- app/BusinessModule/presenters/ClientsPresenter.php
+++ app/BusinessModule/presenters/ClientsPresenter.php
@@ -9,6 +9,7 @@ use App\model\DatagridManager;
 use App\repository\ClientRepository;
 use Nette\Application\AbortException;
 use Nette\Application\UI\Form;
+use Nette\Forms\Container;
 use Nette\Security\User;
 use Nextras\Datagrid\Datagrid;
 
@@ -81,8 +82,87 @@ final class ClientsPresenter extends BasePresenter
         $grid->addColumn('cin', 'IČ');
         $grid->addColumn('email', 'E-mail')->enableSort();
         $grid->addColumn('phone', 'Telefon');
-        $grid->addColumn('address', 'Adresa');
+        $grid->addColumn('street', 'Ulice a č.p.');
+        $grid->addColumn('city', 'Město');
+        $grid->addColumn('zip', 'PSČ');
+
+        $grid->setFilterFormFactory([$this, 'datagridFilterFormFactory']);
+
+        $grid->setEditFormFactory([$this, 'datagridEditFormFactory']);
+        $grid->setEditFormCallback([$this, 'editFormSucceeded']);
 
         return $grid;
+    }
+
+    public function datagridFilterFormFactory(): Container
+    {
+        $form = new Container();
+        $form->addText('name')
+            ->setHtmlAttribute('placeholder', 'Jméno a příjmení');
+
+        $form->addText('cin')
+            ->setHtmlAttribute('placeholder', 'IČ');
+
+        $form->addText('email') //must be text!
+            ->setHtmlAttribute('placeholder', 'E-mail');
+
+        $form->addText('phone', 'Telefon')
+            ->setHtmlAttribute('placeholder', 'Telefon');
+
+        $form->addText('street', 'Ulice a č.p.')
+            ->setHtmlAttribute('placeholder', 'Ulice a č.p.');
+
+        $form->addText('city', 'Město')
+            ->setHtmlAttribute('placeholder', 'Město');
+
+        $form->addText('zip', 'PSČ')
+            ->setHtmlAttribute('placeholder', 'PSČ');
+
+        $form->addSubmit('filter', 'Filtrovat');
+        $form->addSubmit('cancel', 'Zrušit');
+
+        return $form;
+    }
+
+    public function datagridEditFormFactory($row): Container
+    {
+        $form = new Container();
+        $form->addText('name')
+            ->setRequired()
+            ->setHtmlAttribute('placeholder', 'Jméno a příjmení');
+
+        $form->addText('cin')
+            ->setHtmlAttribute('placeholder', 'IČ');
+
+        $form->addEmail('email')
+            ->setHtmlAttribute('placeholder', 'E-mail');
+
+        $form->addText('phone')
+            ->setHtmlAttribute('placeholder', 'Telefon');
+
+        $form->addText('street', 'Ulice a č.p.')
+            ->setHtmlAttribute('placeholder', 'Ulice a č.p.');
+
+        $form->addText('city', 'Město')
+            ->setHtmlAttribute('placeholder', 'Město');
+
+        $form->addText('zip', 'PSČ')
+            ->setHtmlAttribute('placeholder', 'PSČ');
+
+        $form->addSubmit('save', 'Uložit');
+        $form->addSubmit('cancel', 'Zrušit');
+
+        if ($row) {
+            $form->setDefaults($row);
+        }
+        return $form;
+    }
+
+    public function editFormSucceeded(Container $form)
+    {
+        //TODO: proces edit form
+
+        $this->flashMessage('Uloženo', 'success');
+        $this->redrawControl('flashes');
     }
 }
