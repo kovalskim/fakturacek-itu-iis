91a187f5ef736bcdeb59fdf188e379f4426c48aa
Martin Kovalski <martin.kovalski@rekode.cz>
2021-11-24 22:15:51 +0100

    invoices global actions + invoice detail

--- app/BusinessModule/presenters/InvoicingPresenter.php
+++ app/BusinessModule/presenters/InvoicingPresenter.php
@@ -45,20 +45,42 @@ final class InvoicingPresenter extends BasePresenter
         $grid->addColumn('status', 'Status')->enableSort();
         $grid->addColumn('due_date', 'Datum splatnosti')->enableSort();
 
-        $grid->setChangeStatusCallback([$this, 'changeInvoiceStatus']);
+        $grid->setChangeStatusCallback([$this, 'handleChangeInvoiceStatus']);
 
         $grid->setFilterFormFactory([$this, 'datagridFilterFormFactory']);
 
+        $grid->addGlobalAction('paid', 'Zaplaceno', function (array $ids, Datagrid $grid) {
+            foreach ($ids as $id) {
+                $this->invoicingRepository->updateInvoiceStatus($id, 'paid');
+            }
+            $this->flashMessage('Faktury byly označené jako zaplacené', 'success');
+            $this->redrawControl('flashes');
+            $grid->redrawControl('rows');
+        });
+
+        $grid->addGlobalAction('canceled', 'Stornovat', function (array $ids, Datagrid $grid) {
+            foreach ($ids as $id) {
+                $this->invoicingRepository->updateInvoiceStatus($id, 'canceled');
+            }
+            $this->flashMessage('Faktury byly stornovány', 'success');
+            $this->redrawControl('flashes');
+            $grid->redrawControl('rows');
+        });
 
         return $grid;
     }
 
-    public function changeInvoiceStatus($primary, $status)
+    /**
+     * @secured
+     */
+    public function handleChangeInvoiceStatus($primary, $status)
     {
         $this->invoicingRepository->updateInvoiceStatus($primary, $status);
 
         $this->flashMessage('Status byl změněn', 'success');
         $this->redrawControl('flashes');
+        $this->redrawControl('status');
+        $this->redrawControl('status_links');
     }
 
     public function datagridFilterFormFactory(): Container
@@ -81,9 +103,27 @@ final class InvoicingPresenter extends BasePresenter
         ])
             ->setPrompt('--- Status ---');
 
-        $form->addSubmit('filter', 'Filtrovat')->getControlPrototype()->class = 'btn btn-primary';
-        $form->addSubmit('cancel', 'Zrušit')->getControlPrototype()->class = 'btn';
+        $form->addSubmit('filter', 'Filtrovat');
+        $form->addSubmit('cancel', 'Zrušit');
 
         return $form;
+    }
+
+    public function actionInvoice($id)
+    {
+        $user_id = $this->user->getId();
+        $invoice = $this->invoicingRepository->getInvoiceByIdAndUserId($id, $user_id);
+        if(!$invoice)
+        {
+            $this->flashMessage('Požadovaná faktura nebyla nalezena', 'warning');
+            $this->redirect(':Business:Invoicing:default');
+        }
+    }
+
+    public function renderInvoice($id)
+    {
+        $user_id = $this->user->getId();
+        $this->template->invoice = $this->invoicingRepository->getInvoiceByIdAndUserId($id, $user_id);
+        $this->template->invoice_items = $this->invoicingRepository->getInvoiceItemsById($id);
     }
 }
--- app/repository/InvoicingRepository.php
+++ app/repository/InvoicingRepository.php
@@ -4,12 +4,25 @@
 
 namespace App\repository;
 
+use Nextras\Dbal\Result\Row;
+
 class InvoicingRepository extends AllRepository
 {
     private $table = 'invoices';
+    private $table_items = 'invoices_items';
 
     public function updateInvoiceStatus($id, $status)
     {
         $this->connection->query('UPDATE %table SET %set WHERE id = %i', $this->table, ['status' => $status], $id);
+    }
+
+    public function getInvoiceByIdAndUserId($id, $user_id): ?Row
+    {
+        return $this->connection->query('SELECT * FROM %table WHERE id = %i AND users_id = %i', $this->table, $id, $user_id)->fetch();
+    }
+
+    public function getInvoiceItemsById($id): array
+    {
+        return $this->connection->query('SELECT * FROM %table WHERE invoices_id = %i', $this->table_items, $id)->fetchAll();
     }
 }
