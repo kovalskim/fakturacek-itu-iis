b198591dad640924589737fb268f103acd32ed8b
radekjuzl <juzlradek@gmail.com>
2021-12-02 21:35:24 +0100

    New Ares function - parseDataFromAres

--- app/model/AresManager.php
+++ app/model/AresManager.php
@@ -23,4 +23,92 @@ class AresManager
         }
         return 1;
     }
+
+    private function loadData($position, $xml): string
+    {
+        $data = "";
+        while($xml[$position] != '<')
+        {
+            $data .= $xml[$position];
+            $position++;
+        }
+        return $data;
+    }
+
+    /**
+     * @param $cin
+     * @return array|null
+     *
+     */
+    public function parseDataFromAres($cin): ?array
+    {
+        if(is_numeric($cin))
+        {
+            $xml = file_get_contents("https://wwwinfo.mfcr.cz/cgi-bin/ares/darv_std.cgi?ico=".$cin);
+
+            $position = strpos($xml, "Obchodni_firma");
+            if(!($position === false))
+            {
+                $jmeno = $this->loadData($position+15, $xml);
+            }
+            else
+            {
+                return null;
+            }
+
+            $position = strpos($xml, "Nazev_obce");
+            if(!($position === false))
+            {
+                $mesto = $this->loadData($position+11, $xml);
+            }
+            else
+            {
+                return null;
+            }
+
+            $position = strpos($xml, "Nazev_ulice");
+            if(!($position === false))
+            {
+                $ulice = $this->loadData($position+12, $xml);
+            }
+            else
+            {
+                $ulice = $mesto;
+            }
+
+            $position = strpos($xml, "Cislo_domovni");
+            if(!($position === false))
+            {
+                $domovni = $this->loadData($position+14, $xml);
+            }
+            else
+            {
+                return null;
+            }
+
+            $position = strpos($xml, "Cislo_orientacni");
+            if(!($position === false))
+            {
+                $orientacni = "/". ($this->loadData($position+19, $xml));
+            }
+
+            $position = strpos($xml, "PSC");
+            if(!($position === false))
+            {
+                $psc = $this->loadData($position+4, $xml);
+            }
+            else
+            {
+                return null;
+            }
+
+            return [
+                "jmeno" => $jmeno,
+                "mesto" => $mesto,
+                "uliceCislo" => $ulice . " " . $domovni . isset($orientacni),
+                "psc" => $psc
+            ];
+        }
+        return null;
+    }
 }
